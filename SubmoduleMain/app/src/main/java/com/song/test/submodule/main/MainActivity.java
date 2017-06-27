291b272075947ac15b87cb5dc1278d26fe369394
Banton <1039935805@qq.com>
2017-06-27 15:21:00 +0800

    增加 git  submodule

--- SubmoduleMain/app/src/main/java/com/song/test/submodule/main/MainActivity.java
+++ SubmoduleMain/app/src/main/java/com/song/test/submodule/main/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.song.test.submodule.child1.Submodule1Util;
 import com.song.test.submodule.child2.Submodule2Util;
+import com.song.test.submodule.child4.Submodule4Util;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Submodule1Util.test();
         Submodule2Util.test();
+        Submodule4Util.test();
     }
 }
